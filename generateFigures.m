bbec9a9cbfe34c6681910680f115b3ee00a63cb5
Danny Garside <dannygarside@outlook.com>
2025-06-30 18:30:28 +0100

    test NL ellipse computation

--- generateFigures.m
+++ generateFigures.m
@@ -160,6 +160,51 @@ for i = 3:5
     arc_saveFig([saveLocation,'ellipseScatter','_NL_',meta.variableNames{i}],meta)
 end
 
+%% Pick some random hours and plot ellipse and 2D hist
+
+rng(42);
+n = 30;
+randomIndices = when_locationSplit{1,1}.uniqueHourBinIndices(randi(size(when_locationSplit{1,1}.uniqueHourBinIndices,1),[n,1]));
+
+figure, hold on
+tiledlayout(3,10);
+
+for i = 1:length(randomIndices)
+    x = when_locationSplit{1,1}.dataSubset(when_locationSplit{1,1}.hourBinIndices == randomIndices(i),3);
+    y = when_locationSplit{1,1}.dataSubset(when_locationSplit{1,1}.hourBinIndices == randomIndices(i),4);
+
+    meta.figType = "grey";
+    % meta.edges = {linspace(0.66,0.82,40) linspace(0,2,40)};
+    meta.edges = {linspace(0.55,0.9,40) linspace(0,4,40)};
+    specLocus = false; % for some reason having this set to true this makes everything run _very_ slow. TODO investigate
+    nexttile
+    arc_2Dhist(x,y,meta,specLocus);
+
+    center(i,:) = [mean(x,"omitnan"),mean(y,"omitnan")];
+    % EllipseArea(im) = pi * SemiMajorLength(im) * SemiMinorLength(im);
+
+    indind = find(when_locationSplit{1,1}.uniqueHourBinIndices == randomIndices(i));
+    try
+        drawellipse(gca,'SemiAxes',[SemiMajorLength{1,1}(indind),SemiMinorLength{1,1}(indind)],...
+            'Center',center(i,:),...
+            'RotationAngle',360-EllipseAngleUnnormed{1,1}(indind),...
+            'FaceAlpha',0);
+        % colormap('turbo')
+    catch e
+        disp(i)
+        disp(e)
+    end
+
+end
+
+%% HS
+
+% 
+
+
+
+
+
 
 
 
